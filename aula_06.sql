737c39525d5d91c44f3ad242ef3abaa84fb6d1da
GabrielJessi <90281775+GabrielJessi@users.noreply.github.com>
2022-02-10 08:44:35 -0300

    Update aula_06.sql

--- aula_06.sql
+++ aula_06.sql
@@ -37,3 +37,42 @@ SELECT *
 # Outra forma de realizar a ordenação é declarando a posição do campo, ou seja, 
 # a coluna que ele ocupa, ao invés do seu nome. Na nossa tabela, o "id" ocupa a primeira posição, 
 # "matrícula" a segunda e assim sucessivamente.
+
+
+# LIMITANDO AS CONSULTAS
+
+SELECT * FROM funcionarios;
+
+# Imaginemos que só quiséssemos os cinco primeiros resultados.
+# Nesse caso usaremos a cláusula LIMIT para limitamos o retorno.
+
+SELECT * FROM funcionarios LIMIT 5;
+
+SELECT *
+  FROM funcionarios
+  ORDER BY id
+  LIMIT 5
+  OFFSET 1; # ao colocar o 1 estou indicando que quero pular a primeira linha.
+# Se precisarmos do retorno de dados que não estão no começo da tabela, 
+# ou seja, exibir o resultado após avançar algumas linhas, codamos o comando OFFSET . 
+# Essa cláusula pula a quantidade de linhas que estipularmos antes de exibir a busca
+
+# FUNÇÕES DE AGRAGAÇÃO
+
+# Essas são as que mais usamos:
+-- COUNT - Retorna a quantidade de registros
+-- SUM -   Retorna a soma dos registros
+-- MAX -   Retorna o maior valor dos registros
+-- MIN -   Retorna o menor valor dos registros
+-- AVG -   Retorna a média dos registros
+
+SELECT 
+  COUNT(id),
+  SUM(id),
+  MAX(id),
+  MIN(id),
+  ROUND(AVG(id), 2) # O ROUND arredonda, quando informamos 2 dizemos que queremos duas casas decimais. 
+  FROM funcionarios;
+
+# AGRUPANDO CONSULTAS
+
