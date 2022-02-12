447280c367c600290124e9ea6f13bb36fd2c1548
GabrielJessi <90281775+GabrielJessi@users.noreply.github.com>
2022-02-12 13:00:35 -0300

    Update aula_01.sql

--- aula_01.sql
+++ aula_01.sql
@@ -1,46 +1,21 @@
-CREATE TABLE curso(
-	id INTEGER,
-	nome VARCHAR(255)
+-- CREADO TABELA
+
+CREATE DATABASE alura; -- para criar um banco de dados.
+
+DROP DATABASE alura; -- para excluir um banco de dados.
+
+CREATE TABLE aluno(
+        id SERIAL, -- para se impletar automaticamente.
+        nome VARCHAR(255), -- pode ter até 255 caracteres.
+        cpf CHAR(11), -- a gente sabe o tamanho exato do texto que é de 11.
+        observação TEXT, -- texte que não sabemos o tamanho dele.
+        idade INTEGER, -- sem casa decimal.
+        dinheiro NUMERIC(10,2), --(10,2) aqui representa 10 caracteres e duas casas decimais.
+        altura REAL, 
+	ativo BOOLEAN, -- VERDADEIRO ou FALSO.
+        data_nascimento DATE, --tipo data.
+        hora_aula TIME, --tipo TIME representa a horas.
+        matriculado_em TIMESTAMP --
 );
 
-INSERT INTO curso (id, nome) VALUES (NULL, NULL);
-
-SELECT * FROM curso;
-
-DROP TABLE curso;
-
-CREATE TABLE curso(
-	id INTEGER NOT NULL,
-	nome VARCHAR(255) NOT NULL
-);
-
-INSERT INTO curso (id, nome) VALUES (NULL, NULL);
-INSERT INTO curso (id, nome) VALUES (1, NULL);
-INSERT INTO curso (id, nome) VALUES (NULL, 'HTML');
-
-INSERT INTO curso (id, nome) VALUES (1, 'HTML');
-INSERT INTO curso (id, nome) VALUES (1, 'Javascript');
-
-SELECT * FROM curso;
-
-DROP TABLE curso;
-
-CREATE TABLE curso(
-	id INTEGER NOT NULL UNIQUE,
-	nome VARCHAR(255) NOT NULL
-);
-
-INSERT INTO curso (id, nome) VALUES (1, 'HTML');
-INSERT INTO curso (id, nome) VALUES (2, 'Javascript');
-
-DROP TABLE curso;
-
-CREATE TABLE curso(
-	id INTEGER PRIMARY KEY,
-	nome VARCHAR(255) NOT NULL
-);
-
-INSERT INTO curso (id, nome) VALUES (1, 'HTML');
-INSERT INTO curso (id, nome) VALUES (2, 'Javascript');
-
-SELECT * FROM curso;
+SELECT * FROM aluno; -- aqui estamos chamando os dados da tabela.
