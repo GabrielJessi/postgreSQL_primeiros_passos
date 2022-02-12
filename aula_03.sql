89ce7b970c874c93ecab082324b404268a89ac2c
GabrielJessi <90281775+GabrielJessi@users.noreply.github.com>
2022-02-12 15:06:39 -0300

    Update aula_03.sql

--- aula_03.sql
+++ aula_03.sql
@@ -41,3 +41,28 @@ SELECT *
  
  -- FILTRANDO REGISTROS DE CAMPO DO TIPO NUMÉRICO, DATA E BOLEANO. 
  
+ SELECT *
+    FROM aluno
+ WHERE cpf IS NULL; -- busca todos os campos de cpf sem dados ou seja nulos.
+ 
+ SELECT *
+    FROM aluno
+ WHERE idade BETWEEN 10 AND 20; --o BETWEEN é um comando inclusivo. Isso significa que ele começa a contar do menor valor declarado, 
+ --ou seja, o "10", até o maior valor declarado, o "35". 
+ 
+ -- FILTRANDO UTILIZANDO OPERADORES E e OU
+ 
+ SELECT *
+    FROM aluno
+  WHERE nome LIKE 'D%' -- estou friltrando todos que começam com D 
+    AND cpf IS NOT NULL; -- e o campo cpf não estela NULL.
+ -- AND é quando as duas condições são verdadeiras. 
+ 
+ SELECT *
+    FROM aluno
+  WHERE nome LIKE 'Diogo' -- filtra se o nome for Diogo ou Rodrigo.
+    OR nome LIKE 'Rodrigo';
+-- OR é quando uma das condições é verdadeira.
+
+ 
+ 
